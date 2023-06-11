a5c59fe9f565b4f6cb593adad1b1915ae05df909
Sedrak Adoyan <set.adoyan@gmail.com>
2023-06-11 22:32:54 +0400

    feat: add localization

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react'
+
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
+
+import i18n from './i18n'
 
 export default function App() {
+  const [locale, setLocale] = useState<string>(i18n.locale)
+
+  useEffect(() => {
+    i18n.locale = locale
+  }, [locale])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{i18n.t('hello_world')}</Text>
+      <Button
+        onPress={() =>
+          setLocale(prevState => (prevState === 'ru' ? 'en' : 'ru'))
+        }
+        title={i18n.locale}
+      />
+
       <StatusBar style='auto' />
     </View>
   )
